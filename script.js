27bbf6ddb5f4793c49cfa461d50c25c184d3cdb7
jenniferthorpe <jennifer.thorpe@hotmail.com>
2018-10-05 14:48:52 +0200

    js klar

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ $(function() {
 }
 
 $('.addButton').click(addOption);
+
+var optionList = []
+var submit = function() {
+  $(".optionBox").hide();
+  $(".result").css("display", "flex");
+  $("input").each(function(){
+    optionList.push($(this).val())
+  })
+  var winner = optionList[Math.floor(Math.random() * optionList.length)]
+  console.log(winner)
+  $(".result").text(winner)
+}
+$(".goButton").click(submit)
 })
